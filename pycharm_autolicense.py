fe953dae15a81589794ebce40617108b2235c985
Denis Davydov <wirr@wirr.pro>
2018-07-11 00:10:56 +0300

    add requirements.txt

--- pycharm_autolicense.py
+++ pycharm_autolicense.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/venv python
 import os.path
 import sys
 import requests
@@ -6,47 +6,42 @@ import subprocess
 from tkinter import messagebox
 
 
-_API = 'https://api.ideaserver.pro/v1/domain'
+_API = "https://api.ideaserver.pro/v1/domain"
 _SERVER = requests.get(_API).json()
 
 
 def get_url():
     url = _SERVER.get("domain")
-    return f'http://{url}'
+    return f"http://{url}"
+
 
 def ide_choice(ide):
-    return {
-        'pycharm': 'pycharm-professional',
-        'goland': 'goland',
-    }.get(ide, None)
+    return {"pycharm": "pycharm-professional", "goland": "goland"}.get(ide, None)
+
 
 def folder_choice(ide):
-    return {
-        'pycharm': 'PyCharm',
-        'goland': 'GoLand'
-    }.get(ide.lower(), None)
+    return {"pycharm": "PyCharm", "goland": "GoLand"}.get(ide.lower(), None)
+
 
 def get_path(ide):
     keyfile = folder_choice(ide)
     list_files = os.listdir(path=os.getcwd())
-    pycharm_root = list(filter(
-        lambda x: f'{keyfile}' in x, list_files)
-    )[0]
-    key_path = os.path.join(os.getcwd(), pycharm_root, f'config/{ide}.key')
+    pycharm_root = list(filter(lambda x: f"{keyfile}" in x, list_files))[0]
+    key_path = os.path.join(os.getcwd(), pycharm_root, f"config/{ide}.key")
     return key_path
+
 
 def main(ide):
     try:
-        with open(get_path(ide), 'w') as keyfile:
-            keyfile.write(f'URL:{get_url()}')
+        with open(get_path(ide), "w") as keyfile:
+            keyfile.write(f"URL:{get_url()}")
         subprocess.Popen(f"{ide_choice(ide)}")
     except:
         messagebox.showerror(
             "Warning", f"First you must run {ide}! Configuration folder not found!"
-            )
+        )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     ide = sys.argv[1]
     main(ide)
