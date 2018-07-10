c74c363a1b5290890ebad540a9ae02f4f2554fd6
Denis Davydov <wirr@wirr.pro>
2018-07-10 23:50:50 +0300

    add golang support

--- pycharm_autolicense.py
+++ pycharm_autolicense.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3.6
 import os.path
+import sys
 import requests
 import subprocess
+from tkinter import messagebox
 
 
 _API = 'https://api.ideaserver.pro/v1/domain'
@@ -12,22 +14,39 @@ def get_url():
     url = _SERVER.get("domain")
     return f'http://{url}'
 
-def get_path():
+def ide_choice(ide):
+    return {
+        'pycharm': 'pycharm-professional',
+        'goland': 'goland',
+    }.get(ide, None)
+
+def folder_choice(ide):
+    return {
+        'pycharm': 'PyCharm',
+        'goland': 'GoLand'
+    }.get(ide.lower(), None)
+
+def get_path(ide):
+    keyfile = folder_choice(ide)
     list_files = os.listdir(path=os.getcwd())
     pycharm_root = list(filter(
-        lambda x: '{}'.format('PyCharm') in x, list_files)
+        lambda x: f'{keyfile}' in x, list_files)
     )[0]
-    key_path = os.path.join(os.getcwd(), pycharm_root, 'config/pycharm.key')
+    key_path = os.path.join(os.getcwd(), pycharm_root, f'config/{ide}.key')
     return key_path
 
-def main():
+def main(ide):
     try:
-        with open(get_path(), 'w') as keyfile:
+        with open(get_path(ide), 'w') as keyfile:
             keyfile.write(f'URL:{get_url()}')
-        subprocess.Popen("pycharm-professional")
+        subprocess.Popen(f"{ide_choice(ide)}")
     except:
-        print("First, you must run Pycharm, settings folder not found!")
+        messagebox.showerror(
+            "Warning", f"First you must run {ide}! Configuration folder not found!"
+            )
+
 
 
 if __name__ == '__main__':
-    main()
+    ide = sys.argv[1]
+    main(ide)
